55cbd90255ce2aa4d813ba1b0aa07bc26786d3e0
NassimBadaoui <nassim.badaoui@hotmail.fr>
2022-05-11 17:39:41 +0200

    ajout de la sécurite sur l email et le password

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -2,6 +2,12 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// emailregex.com
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
+//Password expression. Password must be between 4 and 8 digits long and include at least one numeric digit. https://regexlib.com/Search.aspx?k=password&c=-1&m=-1&ps=20
+
+
 // Routes
 
 module.exports = {
@@ -16,6 +22,21 @@ module.exports = {
         if(email == null || username == null || password == null) {
             return res.status(400).json({'error' : 'missing parameters'});
         }
+
+        if(username.length >= 13 || username.length <= 4) {
+            return res.status(400).json({'error' : 'wrong username (must be length 5 -12 characters'});
+        }
+
+        if(!EMAIL_REGEX.test(email)) {
+            return res.status(400).json({'error' : 'email is not valid'});
+
+        }
+
+        if(!PASSWORD_REGEX.test(password)) {
+            return res.status(400).json({'error' : 'password invalid (Password must be between 4 and 8 digits long and include at least one numeric digit.)'});
+
+        }
+
 
         //TODO verify pseudo lengt, mail regex, password etc ..
 
